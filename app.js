dd77911c858449890311c31a9c60429035d7ed70
Lu Iribarren <luchieukene@gmail.com>
2022-07-22 21:05:52 -0300

    js, hbs y demas

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var HacemosRouter = require('./routes/Hacemos') ;
+var AnimalesRouter = require('./routes/Animales') ;
+var NovedadesRouter = require('./routes/Novedades') ;
+var ContactoRouter = require('./routes/Contacto') ;
 
 var app = express();
 
@@ -20,7 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/Hacemos', HacemosRouter)
+app.use('/Animales', AnimalesRouter)
+app.use('/Novedades', NovedadesRouter)
+app.use('/Contacto', ContactoRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Animales.js
+++ routes/Animales.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{
-    isHome: true
+    res.render('Animales',{
+      isAnimales: true
+    });
   });
-});
 
 module.exports = router;
