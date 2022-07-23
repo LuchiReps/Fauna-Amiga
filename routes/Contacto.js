df9ef9369fe7853498169bd186b2a70eeec0ad79
Lu Iribarren <luchieukene@gmail.com>
2022-07-23 15:17:59 -0300

    envio de formulario

--- routes/Contacto.js
+++ routes/Contacto.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.render('Contacto',{
-      isContacto: true
-    });
+var nodemailer = require('nodemailer')
+router.get('/', function (req, res, next) {
+  res.render('Contacto', {
+    isContacto: true
   });
+});
+
+router.post('/', async function (req, res, next) {
+  console.log(req.body)
+  var nombre = req.body.nombre
+  var email = req.body.email
+  var tel = req.body.tel
+  var comentario = req.body.comentario
+
+  var obj = {
+    to: 'luchieukene@gmail.com',
+    subject: 'Contacto desde la pagina web',
+    html: nombre + ' se contacto a traves de ' + email + ' ,con un telefono que es ' + tel + ' .Ademas de haber comentado: ' + comentario + '.'
+  }
+var transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+}
+})
+
+var info = await transport.sendMail(obj);
+
+res.render('contacto',{
+message: 'Mensaje enviado correctamente',
+isContacto: true
+})
+
+})
 
 module.exports = router;
