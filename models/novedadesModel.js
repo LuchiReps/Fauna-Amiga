214f971c78e5d46b6c59d097c0fb5543f3a6e949
Lu Iribarren <luchieukene@gmail.com>
2022-07-27 17:47:51 -0300

    agregar novedades

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -6,26 +6,21 @@ async function getNovedades() {
     return rows;
 }
 
-async function insertNovedades(obj){
+async function insertNovedades(obj) {
     try {
 
         var query = 'insert into novedades set ?';
-        var rows = await pool.query(query,[obj]);
+        var rows = await pool.query(query, [obj]);
         return rows;
 
     } catch (error) {
         console.log(error);
         throw error;
     }
-
-   
- async function deleteNovedadByID(id){
-    var query = 'delete from novedades where id= ?';
-    var rows = await pool.query(query,[id]);
-    return rows;
- }
-
+    async function deleteNovedadByID (id) {
+        var query = 'delete from novedades where id= ?';
+        var rows = await pool.query(query, [id]);
+        return rows;
+    }
 }
-
 module.exports = { getNovedades, insertNovedades, deleteNovedadByID }
-   
