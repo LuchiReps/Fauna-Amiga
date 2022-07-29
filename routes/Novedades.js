a4f1967ff2b2c1fc1ee0263d8dd457371373cade
Lu Iribarren <luchieukene@gmail.com>
2022-07-28 21:44:14 -0300

    buscador en front

--- routes/Novedades.js
+++ routes/Novedades.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var novedadesModel = require('../models/novedadesModel') 
+var novedadesModel = require('../models/novedadesModel')
 
-router.get('/', async function(req, res, next) {
-   var novedades = await novedadesModel.getNovedades();
+router.get('/', async function (req, res, next) {
+  //var novedades = await novedadesModel.getNovedades();
 
-  
-  res.render('Novedades',{
-      isNovedades: true,
-      novedades
-    });
+  var novedades;
+  if (req.query.q === undefined) {
+    novedades = await novedadesModel.getNovedades();
+  }else{
+    novedades = await novedadesModel.buscarNovedades(req.query.q);
+
+  }
+
+  res.render('Novedades', {
+    isNovedades: true,
+    novedades,
+    is_search:req.query.q !== undefined,
+    q:req.query.q
   });
+});
 
 module.exports = router;
